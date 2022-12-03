aa84ca679e3723c9c3210ca67aa9dd5869ade27a
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-12-03 23:42:20 +0100

    feat: adding notifications base

--- Domain/Entities/VaccineContext/BaseEntity.cs
+++ Domain/Entities/VaccineContext/BaseEntity.cs
@@ -1,9 +1,11 @@
-using System;
+using Domain.Notifications;
+using Domain.Validations.Interfaces;
 
 namespace Domain.Entities.VaccineContext
 {
-    public class BaseEntity
+    public abstract class BaseEntity : IValidator
     {
+        private List<Notification> _notifications;
         public BaseEntity(string description)
         {
             Id = Guid.NewGuid();
@@ -15,9 +17,18 @@ namespace Domain.Entities.VaccineContext
         public string Description { get; private set; }
         public DateTime CreatedAt { get; private set; }
 
+        public IReadOnlyCollection<Notification> Notifications => _notifications;
+
+        protected void SetNotificationsList(List<Notification> notifications)
+        {
+            _notifications = notifications;
+        }
+
         public virtual void SetDescription(string description)
         {
             Description = description;
         }
+
+        public abstract bool Validate();
     }
 }
